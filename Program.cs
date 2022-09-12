f98337176bfbf9da4330227e0c6e75eba79a3e4f
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-09-12 12:22:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje5._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string uriName ;
+
+            uriName = Console.ReadLine();
+
+
+            Uri uriResult;
+            bool result = Uri.TryCreate(uriName, UriKind.Absolute, out uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+            Console.WriteLine("This Url is valid {0}", result);
+
+            Console.ReadKey();
+
+        }
+    }
+}
